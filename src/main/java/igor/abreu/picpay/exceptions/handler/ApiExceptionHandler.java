5ce93c914593392f1aa8a5a35a2877bc970416c4
Igor Nascimento <igor.nacimento51@gmail.com>
2024-05-23 13:35:24 -0300

    feat: handling validation

--- src/main/java/igor/abreu/picpay/exceptions/handler/ApiExceptionHandler.java
+++ src/main/java/igor/abreu/picpay/exceptions/handler/ApiExceptionHandler.java
@@ -2,7 +2,9 @@ package igor.abreu.picpay.exceptions.handler;
 
 import igor.abreu.picpay.exceptions.PicPayException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,4 +19,22 @@ public class ApiExceptionHandler {
         return ex.toProblemDetail();
     }
 
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public final ProblemDetail handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex
+    ) {
+
+        var fieldErrors = ex.getFieldErrors()
+                .stream()
+                .map(f -> new InvalidParam(f.getField(), f.getDefaultMessage()))
+                .toList();
+
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pb.setTitle("Your request parameters didn't validate.");
+        pb.setProperty("invalid-params", fieldErrors);
+
+        return pb;
+    }
+
+    private record InvalidParam(String fieldName, String reason){}
 }
